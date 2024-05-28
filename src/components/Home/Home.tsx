537f04387e7daa2acf7a808e672ec9c497b55ccc
Felipe-alv-es <brago.sama@gmail.com>
2024-05-27 23:49:19 -0300

    Website base finished

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <Typography variant="h1" fontFamily={"kanit"} fontWeight={"500"}>
+        Home
+      </Typography>
+    </div>
+  );
+};
+
+export default Home;
