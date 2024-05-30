2b1a0cee9ae9dd61eaf31f31221de453eb60616b
Felipe-alv-es <brago.sama@gmail.com>
2024-05-29 22:14:08 -0300

    finished about page base

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./components/Home/Home.tsx";
 import Navbar from "./components/Navbar/Navbar.tsx";
+import About from "./components/About/About.tsx";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <About />
     </div>
   );
 }
---
+++ src/components/About/About.tsx
@@ -0,0 +1,231 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+const About = () => {
+  return (
+    <Box sx={{ justifyContent: "center", display: "flex" }}>
+      <Box
+        sx={{
+          padding: "10%",
+          width: "70%",
+        }}
+      >
+        <Box
+          sx={{
+            width: "70%",
+            marginBottom: "32px",
+            display: "flex",
+          }}
+        >
+          <Typography
+            variant="h2"
+            fontFamily={"kanit"}
+            fontWeight={"500"}
+            color={"#383838"}
+            sx={{ whiteSpace: "pre-wrap", marginBottom: "16px" }}
+          >
+            {"Desenvolva seus produtos rapidamente"}
+          </Typography>
+          <Typography
+            variant="h6"
+            fontFamily={"kanit"}
+            fontWeight={"300"}
+            color={"#383838"}
+            sx={{ whiteSpace: "pre-wrap", marginBottom: "16px" }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            position: "sticky",
+            top: 0,
+            width: "100%",
+            paddingTop: "80px",
+            display: "flex",
+          }}
+        >
+          <Box
+            component="img"
+            src={
+              "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/7e71eb176809137.64cb42810a4cb.jpg"
+            }
+            alt={"Alt teste"}
+            sx={{
+              width: "50%",
+              borderRadius: "16px",
+              opacity: 0,
+            }}
+          />
+          <Box
+            component="img"
+            src={
+              "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/7e71eb176809137.64cb42810a4cb.jpg"
+            }
+            alt={"Alt teste"}
+            sx={{
+              width: "50%",
+              borderRadius: "16px",
+            }}
+          />
+        </Box>
+        <Box
+          sx={{
+            width: "50%",
+            marginBottom: "50%",
+          }}
+        >
+          <Typography
+            variant="h3"
+            fontFamily={"kanit"}
+            fontWeight={"500"}
+            color={"#383838"}
+            sx={{ whiteSpace: "pre-wrap", marginBottom: "16px" }}
+          >
+            {"Desenvolva seus produtos rapidamente"}
+          </Typography>
+          <Typography
+            variant="h6"
+            fontFamily={"kanit"}
+            fontWeight={"300"}
+            color={"#383838"}
+            sx={{ whiteSpace: "pre-wrap", marginBottom: "16px" }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            width: "50%",
+            marginBottom: "50%",
+          }}
+        >
+          <Typography
+            variant="h3"
+            fontFamily={"kanit"}
+            fontWeight={"500"}
+            color={"#383838"}
+            sx={{ whiteSpace: "pre-wrap", marginBottom: "16px" }}
+          >
+            {"Desenvolva seus produtos rapidamente"}
+          </Typography>
+          <Typography
+            variant="h6"
+            fontFamily={"kanit"}
+            fontWeight={"300"}
+            color={"#383838"}
+            sx={{ whiteSpace: "pre-wrap", marginBottom: "16px" }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            width: "50%",
+            marginBottom: "50%",
+          }}
+        >
+          <Typography
+            variant="h3"
+            fontFamily={"kanit"}
+            fontWeight={"500"}
+            color={"#383838"}
+            sx={{ whiteSpace: "pre-wrap", marginBottom: "16px" }}
+          >
+            {"Desenvolva seus produtos rapidamente"}
+          </Typography>
+          <Typography
+            variant="h6"
+            fontFamily={"kanit"}
+            fontWeight={"300"}
+            color={"#383838"}
+            sx={{ whiteSpace: "pre-wrap", marginBottom: "16px" }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            width: "50%",
+            marginBottom: "50%",
+          }}
+        >
+          <Typography
+            variant="h3"
+            fontFamily={"kanit"}
+            fontWeight={"500"}
+            color={"#383838"}
+            sx={{ whiteSpace: "pre-wrap", marginBottom: "16px" }}
+          >
+            {"Desenvolva seus produtos rapidamente"}
+          </Typography>
+          <Typography
+            variant="h6"
+            fontFamily={"kanit"}
+            fontWeight={"300"}
+            color={"#383838"}
+            sx={{ whiteSpace: "pre-wrap", marginBottom: "16px" }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            width: "50%",
+          }}
+        >
+          <Typography
+            variant="h3"
+            fontFamily={"kanit"}
+            fontWeight={"500"}
+            color={"#383838"}
+            sx={{ whiteSpace: "pre-wrap", marginBottom: "16px" }}
+          >
+            {"Desenvolva seus produtos rapidamente"}
+          </Typography>
+          <Typography
+            variant="h6"
+            fontFamily={"kanit"}
+            fontWeight={"300"}
+            color={"#383838"}
+            sx={{ whiteSpace: "pre-wrap", marginBottom: "16px" }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default About;
